7b0733de8cd24d9a5758f69b53b7abf27c73d16c
Patrick Wehrheim <patrick.wehrheim@dbh.de>
2020-06-06 11:42:47 +0200

    Add Costumer and Porosnacks

---
+++ LenasPoroTabelle/Costumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenasPoroTabelle
+{
+    public class Costumer
+    {
+        public Costumer(string name, Porosnacks porosnacks)
+        {
+            Name = name;
+            Porosnacks = porosnacks;
+        }
+
+        public string Name { get; set; }
+
+        public Porosnacks Porosnacks { get; set; }
+    }
+}
---
+++ LenasPoroTabelle/Porosnacks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenasPoroTabelle
+{
+    public class Porosnacks
+    {
+        public Porosnacks(int anzahl)
+        {
+            Anzahl = anzahl;
+        }
+
+        public int Anzahl { get; set; }
+    }
+}
